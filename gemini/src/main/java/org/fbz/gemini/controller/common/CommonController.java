9162ddcfba79ce95e023eec2b1b533d873971d62
fengbozhi92 <18241803952@163.com>
2017-11-29 17:47:48 +0800

    gemini1129C

--- gemini/src/main/java/org/fbz/gemini/controller/common/CommonController.java
+++ gemini/src/main/java/org/fbz/gemini/controller/common/CommonController.java
@@ -1,5 +1,7 @@
 package org.fbz.gemini.controller.common;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CommonController {
     
     @RequestMapping(value="/")
-    public String index(Model model){
-        model.addAttribute("name", "gemini");
+    public String index(Model model, HttpSession session){
         return "views/index";
+    }
+    
+    @RequestMapping(value="/test")
+    public String test(Model model){
+        model.addAttribute("name", "gemini");
+        return "views/test";
     }
 }
